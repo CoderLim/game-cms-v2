daf69a587da38c8d1ab94b288465389df2173b59
Lim <779793115@qq.com>
2026-09-17 21:37:34 +0800

    feat(game-engine): enable per-site analytics on game pages

--- src/routes/game/$slug.tsx
+++ src/routes/game/$slug.tsx
@@ -245,7 +245,9 @@ function GamePage() {
       </main>
       <SiteFooter
         siteName={site.name}
-        socialLinks={publicConfig.socialLinks as any[]}
+        socialLinks={publicConfig.socialLinks}
+        analytics={publicConfig.analytics}
+        ads={publicConfig.ads}
       />
     </div>
   );
