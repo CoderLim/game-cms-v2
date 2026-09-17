532ead06b5728d4d3e25c523029abb9d1d86e1e4
Lim <779793115@qq.com>
2026-09-17 21:38:45 +0800

    feat(game-engine): enable per-site runtime on article pages

--- src/routes/blog/$slug.tsx
+++ src/routes/blog/$slug.tsx
@@ -204,7 +204,9 @@ function BlogPostPage() {
       </main>
       <SiteFooter
         siteName={site.name}
-        socialLinks={publicConfig.socialLinks as any[]}
+        socialLinks={publicConfig.socialLinks}
+        analytics={publicConfig.analytics}
+        ads={publicConfig.ads}
       />
     </div>
   );
